1727aa643978a2ba19e669a2666ad6289aa415bd
楊岳錩 Yueh Chang Yang <52643603+wilson1304983@users.noreply.github.com>
2020-06-03 12:19:14 +0800

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -1,12 +1,5 @@
 import pygame as pg
 import random
-pg.init()
-window_size = (1280,720)
-screen = pg.display.set_mode(window_size)
-screen.fill((0,0,0))
-pg.display.set_caption('攻城獅')
-done = False
-pause = False
 
 class BallSprite(pg.sprite.Sprite):
     def __init__(self):
@@ -14,7 +7,7 @@ class BallSprite(pg.sprite.Sprite):
         self.image = pg.image.load('images/smallball.png')
         self.rect = self.image.get_rect()
         self.rect.center = [640,360] #發球位置 感覺發球要做在迴圈中 按某個鍵後發球 @吳
-        self.xStep, self.yStep = (1,1) #球開始的方向 要依照上局誰得分來覺得 @丁
+        self.xStep, self.yStep = (-1,1) #球開始的方向 要依照上局誰得分來覺得 @丁
     def update(self):
         # move the ball horizontally
         self.rect.x += self.xStep
@@ -31,27 +24,28 @@ class BlockSprite(pg.sprite.Sprite):
         self.image.fill((255,255,255))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        
+pg.init()
+window_size = (1280,720)
+screen = pg.display.set_mode(window_size)
+screen.fill((0,0,0))
+pg.display.set_caption('攻城獅')
+     
 #球        
 ball = BallSprite()
 #牆
 WALL_SIZE = 10
-top_line = BlockSprite(0, 0, window_size[0],
-WALL_SIZE)
-bottom_line = BlockSprite(0, window_size[1]-WALL_SIZE,
-window_size[0], WALL_SIZE)
-left_line = BlockSprite(0, 0, WALL_SIZE,
-window_size[1])
-right_line = BlockSprite(window_size[0]-WALL_SIZE, 0,
-WALL_SIZE, window_size[1])
+top_line = BlockSprite(0, 0, window_size[0],WALL_SIZE)
+bottom_line = BlockSprite(0, window_size[1]-WALL_SIZE,window_size[0], WALL_SIZE)
+left_line = BlockSprite(0, 0, WALL_SIZE,window_size[1])
+right_line = BlockSprite(window_size[0]-WALL_SIZE, 0,WALL_SIZE, window_size[1])
 
 #群組
 horiz_walls = pg.sprite.Group(top_line, bottom_line)
 vert_walls = pg.sprite.Group(left_line, right_line)
 balls = pg.sprite.Group(ball)
-sprites = pg.sprite.OrderedUpdates(horiz_walls,
-vert_walls, balls)  
-
+sprites = pg.sprite.OrderedUpdates(horiz_walls,vert_walls, balls)  
+done = False
+pause = False   
 while not done:
 # read new event
     for event in pg.event.get():
@@ -59,12 +53,17 @@ while not done:
                 done = True
             if (event.type == pg.KEYUP and event.key == pg.K_ESCAPE):
                 done = True
+            if ball.rect.x: #觸發事件
+                new_ball = BallSprite()
+                new_ball.rect.center = [ball.rect.x,ball.rect.y]
+                #加進群組
 #update game state
 #redraw
-    pg.display.update()
+    screen.fill((0,0,0))
+    balls.update()
     sprites.draw(screen)
-    ball.update()
-    screen.blit(ball.image, ball.rect)
+    pg.display.update()
+
     while pause:
         for event in pg.event.get():
             if event.type == pg.QUIT:
